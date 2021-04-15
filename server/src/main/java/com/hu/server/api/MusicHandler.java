971da4bf41ddbb6078d960f77059a22fed656088
bighu <1030499842@qq.com>
2021-04-15 09:43:12 +0800

    接口已准备好测试

--- server/src/main/java/com/hu/server/api/MusicHandler.java
+++ server/src/main/java/com/hu/server/api/MusicHandler.java
@@ -16,16 +16,16 @@ public class MusicHandler {
     /**
      * hello world
      */
-    @RequestMapping("/confirm")
-    public ResponseDto<ArrayList<MusicInfo>> confirm(){
-
-        ArrayList<MusicInfo> list = new ArrayList<>();
-        list.add(new MusicInfo());
-        list.add(new MusicInfo());
-        list.add(new MusicInfo());
-
-        return new ResponseDto<>(list);
-    }
+//    @RequestMapping("/confirm")
+//    public ResponseDto<ArrayList<MusicInfo>> confirm(){
+//
+//        ArrayList<MusicInfo> list = new ArrayList<>();
+//        list.add(new MusicInfo());
+//        list.add(new MusicInfo());
+//        list.add(new MusicInfo());
+//
+//        return new ResponseDto<>(list);
+//    }
 
 
     /**
@@ -37,9 +37,16 @@ public class MusicHandler {
     public ResponseDto<ArrayList<MusicInfo>> hotMusic(){
 
         ArrayList<MusicInfo> list = new ArrayList<>();
-        list.add(new MusicInfo());
-        list.add(new MusicInfo());
-        list.add(new MusicInfo());
+        MusicInfo mi = new MusicInfo();
+        mi.setMusicId("1911");
+        mi.setMusicName("hotmusic");
+        mi.setStorePath("/musicServer/等人.m4a");
+        mi.setSinger("hot");
+
+        for (int i=0;i<11;i++){
+            list.add(mi);
+        }
+
 
         return new ResponseDto<>(list);
     }
@@ -54,9 +61,19 @@ public class MusicHandler {
     public ResponseDto<ArrayList<MusicInfo>> recommendMusic(){
 
         ArrayList<MusicInfo> list = new ArrayList<>();
+        MusicInfo mi = new MusicInfo();
+        mi.setMusicId("1912");
+        mi.setMusicName("recommendmusic");
+        mi.setStorePath("/musicServer/等人.m4a");
+        mi.setSinger("recommend");
+
+        for (int i=0;i<11;i++){
+            list.add(mi);
+        }
 
 
         return new ResponseDto<>(list);
+
     }
 
 
@@ -64,6 +81,7 @@ public class MusicHandler {
      * 用于收集用户习惯
      * 传入歌曲id
      * 在java中创建埋点日志
+     * 如果前端需要返回该id对应的音乐信息，我也可以返回
      */
     @RequestMapping("/collectInfo")
     public ResponseDto<String> collectInfo(@RequestParam String songId){
